e79b4f8d0295e9e5cf57a1abe4f97a6ebce94fa1
Joan in DC <joanrigdon@gmail.com>
2019-01-30 22:54:01 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,18 +1,24 @@
 
-
 def unsafe?(speed)
-  if speed < 40 || speed > 60
-    return true
-  else
-    return false
-  end
+  speed > 40 ? false : true
 end
 
 
 
-def not_safe?(speed)
-	speed > 40 && speed < 60 ? false : true
-end
+
+# def unsafe?(speed)
+#   if speed < 40 || speed > 60
+#     return true
+#   else
+#     return false
+#   end
+# end
+
+
+
+# def not_safe?(speed)
+# 	speed > 40 && speed < 60 ? false : true
+# end
 	
 
 
